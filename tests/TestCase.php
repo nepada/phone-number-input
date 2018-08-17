64ff753491b484eb3dc04f99e16d835e74932712
Petr Morávek <petr@pada.cz>
2018-08-17 23:32:54 +0200

    Run PHPStan on tests

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -9,6 +9,15 @@ use Tester;
 abstract class TestCase extends Tester\TestCase
 {
 
+    public function run(): void
+    {
+        if ($_ENV['IS_PHPSTAN'] ?? false) {
+            return;
+        }
+
+        parent::run();
+    }
+
     protected function tearDown(): void
     {
         parent::tearDown();
